2a47899d11eff6f309479e3634f062c80eeffc7f
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-01-08 18:43:53 -0500

    ad-hoc

---
+++ codeforces/1056-Div12-A.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stdlib.h>
+#include<math.h>
+using namespace std;
+
+int despert;
+int vist;
+int paradas[101];
+int x;
+
+int main(){
+cin>>despert;
+	for(int i=1; i<=despert; i++){
+	cin>>vist;
+		for(int i=0; i<=vist-1;i++){
+			cin>>x;
+			paradas[x]=paradas[x]+1;
+		}
+	}
+	for(int i=1; i<=100; i++){
+		if(paradas[i]==despert){
+			cout<<i<<" ";
+		}
+	}
+	cout<<endl;
+return 0;
+}
