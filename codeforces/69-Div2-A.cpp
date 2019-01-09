39a8ab4bcfde487886c065694f4606a0eed8d5d2
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-01-09 12:50:12 -0500

    ad-hoc

---
+++ codeforces/69-Div2-A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int x[100];
+int y[100];
+int z[100];
+int n;
+int sumx=0;
+int sumy=0;
+int sumz=0;
+
+int main() {
+	cin>>n;
+	for(int i=1; i<=n; i++){
+		cin>>x[i]>>y[i]>>z[i];
+		sumx=sumx+x[i];
+		sumy=sumy+y[i];
+		sumz=sumz+z[i];
+	}
+	if(sumx==0 && (sumy==0 && sumz==0)){
+		cout<<"YES"<<endl;
+	}else cout<<"NO"<<endl;
+	return 0;
+}
