c58333a0027dcc073c25bbad03750a8e81a2a0cc
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-01-16 09:49:15 -0500

    ad-hoc

--- POJ/1401 - Factorial.cpp
+++ POJ/1401 - Factorial.cpp
@@ -1,9 +1,7 @@
 /*
 *
-* V1.
 * 1401-Factorial:
 * http://poj.org/problem?id=1401
-*
 *
 */
 #include<iostream>
@@ -12,25 +10,22 @@
 using namespace std;
 int T;
 long long M;
+long long sum;
+long long pot5[16];
+long long numb5[16];
 int main(){
     cin>>T;
+    pot5[0]=1;
+	for(int j=1; j<=15;j++){
+     	pot5[j]=pot5[j-1]*5;
+    }
     for(int i=1;i<=T;i++){
-          cin>>M;
-			    long long num5=M/5;
-        	long long num25=M/25;
-        	long long num125=M/125;
-        	long long num625=M/625;
-        	long long num3125=M/3125;
-        	long long num15625=M/15625;
-        	long long num78125=M/78125;
-        	long long num390625=M/390625;
-        	long long num1953125=M/1953125;
-        	long long num9765625=M/9765625;
-        	long long num48828125=M/48828125;
-        	long long num244140625=M/244140625;
-        	long long num1220703125=M/1220703125;
-        	long long sum=num5+num25+num125+num625+num3125+num15625+num78125+num390625+num1953125+num9765625+num48828125+num244140625+num1220703125;
-    
+        sum=0;
+        cin>>M;
+        for(int j=1; j<=15; j++){
+        	numb5[j]=M/pot5[j];
+        	sum=sum+numb5[j];
+        }
     	cout<<sum<<endl;
     }
 }
