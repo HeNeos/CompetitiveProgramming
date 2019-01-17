884871c487a4e9413c05ed5c3655998f108c36f5
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-01-17 07:26:04 -0500

    ad-hoc

---
+++ codeforces/318-Div2-C.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long a;
+long long b;
+long long m;
+long long cont;
+long long suma;
+int main(){
+	cin>>a>>b>>m;
+	if(m>max(a,b) && b<=0 && a<=0){
+		cout<<-1<<endl;
+		goto salir;
+	}
+	suma=a+b;
+	b=max(a,b);
+	a=suma-b;
+	if(b>=m){
+		cout<<0<<endl;
+		goto salir;
+	}
+	if(a<0){
+    long long k=(-a)/b;
+    cont=k;
+    a+=k*b;
+  }
+	for(long long i=0; i<=1000000;i++){
+		suma=a+b;
+		b=max(a,b);
+		a=suma-b;
+		if(b>=m){
+			cout<<cont<<endl;
+			goto salir;
+		}
+		cont++;
+		a=suma;
+	}
+	salir:;
+}
