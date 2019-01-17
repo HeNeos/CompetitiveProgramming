6c3d37ccf41c453c01db18630020db8e98fe26b3
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-01-17 04:52:55 -0500

    ad-hoc

---
+++ codeforces/552-Div2-B.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long N;
+int cont=1;
+long long aux;
+int main(){
+	cin>>N;
+	aux=N;
+	for(int i=1; i<=10; i++){
+		if(aux<10){
+			break;
+		}
+		aux=aux/10;
+		cont++;
+	}
+	long long sum=0;
+	for(int i=1; i<=cont-1; i++){
+		sum=sum+9*pow(10,i-1)*i;
+	}
+	sum=sum+cont*(1+N-pow(10,cont-1));
+	cout<<sum;
+}
