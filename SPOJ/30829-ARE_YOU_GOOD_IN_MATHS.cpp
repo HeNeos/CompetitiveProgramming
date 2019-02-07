44d47865ed2fa189aacf202382f6a97561b358c6
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-02-07 01:56:05 -0500

    ad-hoc

---
+++ SPOJ/30829-ARE_YOU_GOOD_IN_MATHS.cpp
@@ -0,0 +1,29 @@
+/*
+*
+* ARE YOU GOOD IN MATHS?
+* https://www.spoj.com/problems/BABA1/
+*
+/*
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int T;
+	cin>>T;
+	for(int i=1; i<=T; i++){
+		double A;
+		double H;
+		cin>>H>>A;
+		if(H*H-4.0*A<0){
+			cout<<"-1"<<endl;
+		}
+		else{
+			double sum=sqrt(H*H+4.0*A);
+			double resta=sqrt(H*H-4.0*A);
+			double lado1=(sum-resta)/2.0;
+			double lado2=sum-lado1;
+			if(lado1>lado2) swap(lado1,lado2);
+			printf("%.6lf %.6lf %.6lf \n",lado1,lado2,H);
+		}
+	}
+}
