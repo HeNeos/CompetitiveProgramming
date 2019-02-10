584ccfb37797973047b232b29ac921a6c2a4d970
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-02-10 00:40:48 -0500

    ad-hoc

---
+++ codeforces/320-Div2-A.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	long long n;
+	int cifr;
+	cin>>n;
+	cifr=floor(log10(n))+1;
+	int i=0;
+	while(i<cifr){
+		if(n%10==1){
+			n=n/10;
+			i++;
+			continue;
+		}
+		else{
+			if(n%10==4){
+				n=n/10;
+				if(n%10==1){
+					n=n/10;
+					i=i+2;
+					continue;
+				}
+				else{
+					if(n%10==4){
+						n=n/10;
+						if(n%10==1){
+							n=n/10;
+							i=i+3;
+							continue;
+						}
+						else{
+							cout<<"NO";
+							return 0;
+						}
+					}
+				}
+			}
+			else{
+				cout<<"NO";
+				return 0;
+			}
+		}
+	}
+	cout<<"YES";
+	return 0;
+}
