22656eff1ad54651501730d632312b4481086f73
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-02-10 01:06:45 -0500

    ad-hoc

---
+++ codeforces/1110-Div2-A.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int b;
+	int k;
+	cin>>b>>k;
+	int arr[k];
+	int suma=0;
+	for(int i=0; i<k; i++){
+		cin>>arr[i];
+		suma=suma+arr[i];
+	}
+	if(!(b&1)){
+		if((arr[k-1]%2)==0){
+			cout<<"even";
+			return 0;
+		}
+		else{
+			cout<<"odd";
+			return 0;
+		}
+	}
+	else{
+		if(suma%2==0){
+			cout<<"even";
+			return 0;
+		}
+		else{
+			cout<<"odd";
+			return 0;
+		}
+	}
+}
