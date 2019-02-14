e5a1d43fbfb30fb66615d46932b8ccde15fd2371
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-02-14 01:21:11 -0500

    ad-hoc

---
+++ codeforces/1108-Div3-C.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    string si;
+    cin>>si;
+    int vecc[3]={0,1,2};
+    string bse="RBG";
+    int minm=INT_MAX;
+    string strr;
+    do{
+		string comp;
+        int cont=0;
+        for(int i=0;i<N;i++){
+            comp+=bse[vecc[i%3]];
+            if(comp[i]!=si[i]){
+                cont++;
+            }
+        }
+        if(minm>cont){
+            minm=cont;
+            strr=comp;
+        }
+    }while(next_permutation(vecc,vecc+3));
+    cout<<minm<<endl;
+    cout<<strr;
+    return 0;
+}
