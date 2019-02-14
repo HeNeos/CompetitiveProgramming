399b865c723256d7245aeff59dcf893272df0eb9
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-02-14 00:44:49 -0500

    ad-hoc

---
+++ codeforces/677-Div2-A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+int h;
+int alturaper[1001];
+int conth=0;
+int main(){
+	cin>>n>>h;
+	for(int i=1; i<=n; i++){
+		cin>>alturaper[i];
+		if(alturaper[i]>h){
+			conth++;
+		}
+	}
+	cout<<n+conth;
+}
