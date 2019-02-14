ba6c04b12761aedcb6f09061f78e9a5171153eb0
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-02-14 01:25:39 -0500

    ad-hoc

---
+++ UVa/10905.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int N;
+string numb;
+vector <string> arr;
+string aux;
+bool cmp(string a, string b){
+	string x1=a+b;
+	string x2=b+a;
+	if(x1>x2) return(true);
+	else return(false);
+}
+int main(){
+	vector <string> arr2;
+	while(1){
+		cin>>N;
+		arr.clear();
+		if(N==0) break;
+		for(int i=1; i<=N; i++){
+			cin>>numb;
+			arr.push_back(numb);
+		}
+		for(int i=0; i<N; i++){
+			for(int j=0; j<N-1; j++){	
+				if(!(cmp(arr[j],arr[j+1]))) swap(arr[j],arr[j+1]);
+			}
+		}
+		for(int i=0; i<N; i++){
+			cout<<arr[i];
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
