5708145b448184407b445896b79ce88ef035965b
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-02-14 01:00:38 -0500

    ad-hoc

---
+++ codeforces/467-Div2-B.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	int m;
+	int k;
+	int cont = 0;
+	int difsldir;
+	cin >> n >> m >> k;
+	int sldir[m+1];
+	for(int i = 0; i <= m; i++){
+		cin >> sldir[i];
+	}
+	for(int i = 0; i < m; i++){
+		difsldir = sldir[i]^sldir[m];
+		int bitdiff = 0;
+		for(int j = 0; j < n ; j++){
+			if((difsldir>>j)&1){
+				bitdiff++;
+			}
+		}
+		if(bitdiff<=k){
+			cont++;
+		}
+	}
+	cout << cont <<"\n";
+}
