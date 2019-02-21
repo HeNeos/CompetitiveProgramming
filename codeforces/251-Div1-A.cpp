9a158a73c17e72c417f4d961332a2d3a1fe4a110
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-02-20 23:44:27 -0500

    ad-hoc

---
+++ codeforces/251-Div1-A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int d;
+int main(){
+    cin >> n >> d;
+    int a[n+1];
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    long long answ = 0;
+    for (int i = 1; i <= n - 2; i++){
+		int pos = upper_bound(a + 1, a+n+1, a[i] + d) - a;
+		long long dif = pos - i - 1;
+		if (dif >= 1) answ += (dif * (dif - 1) / 2);
+    	}
+    cout << answ << endl;
+    return 0;
+}
