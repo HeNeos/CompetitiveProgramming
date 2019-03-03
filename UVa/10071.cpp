44765c809828cf5bf2bcea9e18bb3cc23489ae20
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-03-03 13:59:00 -0500

    ad-hoc

---
+++ UVa/10071.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+        int v;
+        int t;
+        while(cin >> v >> t){
+                cout << 2*v*t << "\n";
+        }
+        return 0;
+}
