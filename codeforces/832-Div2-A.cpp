65f0b47686f1afd7317e1082763a0539984d6f62
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-06-16 18:57:51 -0500

    ad-hoc

---
+++ codeforces/832-Div2-A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    long long k;
+    cin >> n >> k;
+    if((n/k)%2 == 1) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
