d8b1210794466ad942ae5c8b069a7b97477272ed
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-06-16 19:38:53 -0500

    ad-hoc

---
+++ codeforces/841-Div2-B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	for(int i=0; i<n; i++){
+		int numb;
+		cin >> numb;
+		if(numb&1){
+		    cout << "First";
+		    return 0;
+		}
+	}
+	cout << "Second";
+	return 0;
+}
