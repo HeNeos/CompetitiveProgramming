62a50cb2d60dfe9b9a7e60224a8d9414c7975999
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-11-16 03:00:14 -0500

    :o

---
+++ codeforces/1081-Div12-C.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long prim = 998244353; 
+long long tablee[2002][2002];
+int main(){
+	int n;
+	int m;
+	int k;
+	cin >> n >> m >> k;
+	tablee[1][0] = m;
+	for(int i=1; i<n; i++){
+		for(int j=0; j<=k; j++){
+			tablee[i+1][j] = ((tablee[i][j])%prim + (tablee[i+1][j])%prim)%prim;
+			tablee[i+1][j+1] = ((tablee[i][j]*(m-1))%prim + tablee[i+1][j+1]%prim)%prim;
+		}
+	}
+	cout << (tablee[n][k])%prim;
+	return 0;
+}
