760085718a9420dafe950cbf825b2546f8560999
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-11-16 02:59:17 -0500

    :o

---
+++ codeforces/1005-Div3-B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s1;
+string s2;
+int longstr(int p, int q){
+	if(p<0 or q<0) return 0;
+	if(s1[p] == s2[q]) return 1+longstr(p-1,q-1);
+	else return 0;
+}
+int main(){
+	cin >> s1 >> s2;
+	cout << s1.size()+s2.size()-2*longstr(s1.size()-1, s2.size()-1);
+	return 0;
+}
