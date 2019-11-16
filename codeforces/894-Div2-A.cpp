63dfa454f624550ae5492f4b2c92b0dd25b6fa83
HeNeos <46503611+HeNeos@users.noreply.github.com>
2019-11-16 02:56:38 -0500

    :o

---
+++ codeforces/894-Div2-A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int contq[110];
+int main(){
+	string s;
+	cin >> s;
+	int cont = 0;
+	for(int i=0; i<s.size(); i++){
+		if(s[i] == 'Q') contq[i] = contq[i-1]+1;
+		else contq[i] = contq[i-1];
+	}
+	for(int i=1; i<s.size()-1; i++){
+		if(s[i] == 'A'){
+			cont = cont + contq[i]*(contq[s.size()-1]-contq[i]);
+		}
+	}
+	cout << cont;
+	return 0;
+}
