abc5b78205a49f151c185bbadc8890933bc6fca5
HeNeos <46503611+HeNeos@users.noreply.github.com>
2020-04-23 13:12:25 -0500

    Mobius and inverse mobius

--- CodeChef/Simple_Sum.cpp
+++ CodeChef/Simple_Sum.cpp
@@ -1,38 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int prim[100005];
+int prim[10000005];
 long long fastexp(long long a, long long b){
-	if(b==0) return 1;
-	if(b==1) return a;
-	if(b%2 == 0){
-		long long aux = fastexp(a,b/2);
-		return 1LL*aux*aux;
-	}
-	long long aux = fastexp(a,(b-1)/2);
-	return 1LL*a*aux*aux;
+  if(b==0) return 1;
+  if(b==1) return a;
+  if(b%2 == 0){
+    long long aux = fastexp(a,b/2);
+    return 1LL*aux*aux;
+  }
+  long long aux = fastexp(a,(b-1)/2);
+  return 1LL*a*aux*aux;
 }
-vector <int> rp;
 void lpf(){
-    for(int i=0; i<100005; i++) prim[i] = 0;
-    for(int i=2; i<100005; i++){
+    for(int i=0; i<10000005; i++) prim[i] = 0;
+    for(int i=2; i<10000005; i++){
         if(!prim[i]){
-            for(int j=1; 1LL*j*i<100005; j++){
+            for(int j=1; 1LL*j*i<10000005; j++){
                 if(!prim[1LL*i*j]) prim[1LL*i*j] = i;
             }
-        }
-    }
-	for(int i=2; i<100005; i++){
-		if(prim[i] == i) rp.push_back(i);
-	}
-}
-int mobius[100005];
-void cmob(){
-    mobius[1] = 1;
-    for(int i=2; i<100005; i++){
-        if(prim[i] == i) mobius[i] = -1;
-        else{
-            if(prim[i/prim[i]] == prim[i]) mobius[i] = 0;
-            else mobius[i] = -1*mobius[i/prim[i]];
         }
     }
 }
@@ -40,34 +25,34 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     lpf();
-    cmob();
-	int t;
-	cin >> t;
-	while(t--){
-		int n;
-		cin >> n;
-		vector <int> divisors;
-		for(int i=1; i*i<=n; i++){
-			if(n%i == 0){
-				divisors.push_back(i);
-				if(i != n/i) divisors.push_back(n/i);
-			}
-		}
-		long long answ = 0;
-		for(int i=0; i<divisors.size(); i++){
-			int answ2 = n/divisors[i];
-			int act = n/divisors[i];
-			int cc = prim[act];
-			while(act > 1){
-				cc = prim[act];
-				while(prim[act] == cc){
-					act = act/cc;
-				}
-				answ2 = answ2 - (answ2)/cc;
-			}
-			answ = answ + answ2*(n/divisors[i]);
-		}
-		cout << answ << '\n';
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    if(n==1){
+      cout << 1 << '\n';
+      continue;
+    }
+    vector <pair <long long,long long> > div;
+    int auxn = n;
+    while(auxn>1){
+      int fdiv = prim[auxn];
+      div.push_back(make_pair(fdiv,0));
+      while(prim[auxn]==fdiv){
+        auxn = auxn/fdiv;
+        div[div.size()-1].second++;
+      }
+    }
+    long long answ = 1;
+	for(int i=0; i<div.size(); i++){
+		long long numm = div[i].first;
+		long long expp = div[i].second;
+		long long aux1 = fastexp(numm,2*expp);
+		long long aux2 = fastexp(numm,2*expp-1);
+		answ = 1LL*answ*(aux1-aux1/numm+((aux2+1)/(numm+1)));
 	}
-	return 0;
+    cout << answ << '\n';
+  }
+  return 0;
 }
