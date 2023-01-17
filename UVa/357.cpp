07c89530536681e5c26da2ca128e119986e4d74b
heneos3.14@gmail.com <heneos3.14@gmail.com>
2023-01-16 23:07:07 -0500

    fixed uva problem

--- UVa/357.cpp
+++ UVa/357.cpp
@@ -1,27 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 #define N 30005
-int n;
-ll dp[N];
+using ll = long long;
+ll dp[N][5];
+int c[5] = {1,5,10,25,50};
 
-int change[5] = {1,5,10,25,50};
-
-void solve(){
-    dp[0] = 1;
-    for(int j=0; j<5; j++){
-        for(int i=change[j]; i<N; i++){
-            dp[i] += dp[i-change[j]];
-        }
+ll solve(int n, int p){
+    if(n < 0) return 0;
+    if(n == 0) return 1;
+    if(dp[n][p] != -1) return dp[n][p];
+    ll ans = 0;
+    for(int i=p; i>=0; i--){
+        ans += solve(n-c[i], i);
     }
+    return dp[n][p] = ans;
 }
 
+
 int main(){
-    solve();
+    memset(dp, -1, sizeof dp);
+    int n;
     while(cin >> n){
-        if(dp[n] == 1) cout << "There is only 1 way";
-        else cout << "There are " << dp[n] << " ways";
-        cout << " to produce " << n << " cents change.\n"; 
+    	ll ans = solve(n, 4);
+    	if(ans == 1) cout << "There is only 1 way ";
+    	else cout << "There are " << ans << " ways ";
+    	cout << "to produce " << n << " cents change.\n";
     }
     return 0;
 }
