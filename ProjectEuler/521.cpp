ef22de5c136ad564c898c65fef1768f9eca5d8e5
HeNeos <heneos3.14@gmail.com>
2024-05-05 21:13:39 +0200

    Improve 521
    Use Lucy Algorithm to reduce time complexity

--- ProjectEuler/521.cpp
+++ ProjectEuler/521.cpp
@@ -1,66 +1,77 @@
 #include <bits/stdc++.h>
+
+const int N = 1200000;
+const int MOD = 1000000000;
 using namespace std;
 using ll = long long;
 
-#define N 1500000
-#define MOD 1000000000
+vector<int> prime;
+vector<bool> is_prime(N, true);
+unordered_map<ll, unordered_map<int, int>> dp;
 
-bool is_composite[N];
-vector<int> primes;
 void sieve() {
-  for (int i = 2; i < N; i++) {
-    if (!is_composite[i]) {
-      primes.push_back(i);
-      for (ll j = 1LL * i * i; j < N; j += i)
-        is_composite[j] = true;
+    for (int i = 2; i < N; ++i) {
+        if (is_prime[i]) {
+            prime.push_back(i);
+            for (ll j = 1LL * i * i; j < N; j += i) {
+                is_prime[j] = false;
+            }
+        }
     }
-  }
 }
 
-string to_key(ll n, int index) { return to_string(n) + "#" + to_string(index); }
+int prime_pi(int n) {
+    int lo = 0;
+    int hi = prime.size() - 1;
+    while (lo < hi) {
+        int me = lo + (hi - lo + 1) / 2;
+        if (1LL * prime[me] * prime[me] <= n) {
+            lo = me;
+        } else {
+            hi = me - 1;
+        }
+    }
+    return lo;
+}
 
-#define PI_N 78500
-
-unordered_map<string, int> cache;
-ll f(ll n, int index) {
-  ll d = n / primes[index];
-  ll ans = d % MOD;
-  if (d < 1)
-    return 0;
-  if (index == 0)
-    return ans;
-  if (index == 1)
-    return (d - d / 2) % MOD;
-  if (index == 2)
-    return (d - d / 3 + d / 3 / 2 - d / 2) % MOD;
-  string key = to_key(n, index);
-  if (cache.find(key) != cache.end()) {
-    return cache[key];
-  }
-  for (int i = 0; i < index; i++) {
-    ans -= f(d, i);
+ll S(ll n, int v) {
+    int p = prime[v];
+    ll d = n / p;
+    if (v == 0) {
+        return n - d + 1;
+    }
+    if (1LL * p * p > n) {
+        return S(n, prime_pi(n));
+    }
+    if (dp.find(n) != dp.end() && dp[n].find(v) != dp[n].end()) {
+        return dp[n][v];
+    }
+    ll ans = S(n, v - 1) - (S(d, v - 1) - S(p - 1, v - 1));
     ans %= MOD;
-  }
-  if (ans == 0)
-    ans += MOD;
-  if (cache.size() < 50000000 & index >= 5)
-    cache[key] = ans;
-  return ans;
+    dp[n][v] = ans;
+    return ans;
+}
+
+ll solve(ll n) {
+    ll ans = 2LL * (n / 2) - 2;
+    for (int i = 1; i < prime.size(); ++i) {
+        int p = prime[i];
+        if (1LL * p * p > n) {
+            break;
+        }
+        ll aux = 1LL * p * (S(n / p, i - 1) - S(p - 1, i - 1));
+        aux %= MOD;
+        ans += aux;
+    }
+    return ans;
 }
 
 int main() {
-  sieve();
-  ll n;
-  cin >> n;
-  ll ans = 705911377;
-  ll prime_sum = 0;
-  for (int i = 0; i < PI_N; i++) {
-    if (1LL * primes[i] * primes[i] > n)
-      break;
-    prime_sum += primes[i];
-    int amount = f(n, i) % MOD;
-    ans += (1LL * amount * primes[i]) % MOD;
-  }
-  cout << ((ans - prime_sum) % MOD + MOD) % MOD << '\n';
-  return 0;
+    sieve();
+    ll n = 1000000000000;
+    ll ans = solve(n)%MOD;
+    ans += 705911377;
+    ans %= MOD; ans += MOD; ans %= MOD;
+    cout << ans << '\n';
+    return 0;
 }
