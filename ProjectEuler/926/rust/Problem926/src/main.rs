c2724d6f9872fe2265736a11a914af96329b812a
HeNeos <heneos3.14@gmail.com>
2025-01-19 20:37:39 +0100

    Finish problem 926

--- ProjectEuler/926/rust/Problem926/src/main.rs
+++ ProjectEuler/926/rust/Problem926/src/main.rs
@@ -1,69 +1,56 @@
-use std::cmp;
-use std::collections::HashMap;
-
 const MAX_N: u32 = 10_000_005;
+const MOD: u64 = 1_000_000_007;
 
 fn sieve() -> Vec<u32> {
-    let mut least_prime_factor = vec![0; (MAX_N + 1) as usize];
-    least_prime_factor[1] = 1;
+    let mut is_prime = vec![true; (MAX_N + 1) as usize];
+    let mut primes = Vec::new();
     for number in 2..=MAX_N {
-        if least_prime_factor[number as usize] == 0 {
-            least_prime_factor[number as usize] = number;
+        if is_prime[number as usize] {
+            primes.push(number);
             let mut multiple: u64 = number as u64 * number as u64;
             while multiple <= MAX_N as u64 {
-                if least_prime_factor[multiple as usize] == 0 {
-                    least_prime_factor[multiple as usize] = number;
-                }
+                is_prime[multiple as usize] = false;
                 multiple += number as u64;
             }
         }
     }
-    least_prime_factor
+    primes
 }
 
-fn factorize(mut n: u32, least_prime_factor: &Vec<u32>) -> Vec<(u32, u32)> {
-    let mut prime_factors: Vec<(u32, u32)> = Vec::new();
-    while n > 1 {
-        let divisor = least_prime_factor[n as usize];
-        let mut counter = 0;
-        while n % divisor == 0 {
-            n /= divisor;
-            counter += 1;
+fn factorize_factorial(n: u32, primes: &Vec<u32>) -> Vec<(u32, u32)> {
+    let mut factorization: Vec<(u32, u32)> = Vec::new();
+    for prime in primes {
+        if *prime > n {
+            break;
         }
-        prime_factors.push((divisor, counter));
-    }
-    prime_factors
-}
-
-fn factorize_factorial(n: u32, least_prime_factor: &Vec<u32>) -> HashMap<u32, u32> {
-    let mut factorization: HashMap<u32, u32> = HashMap::new();
-    for number in 2..=n {
-        let number_factors: Vec<(u32, u32)> = factorize(number, least_prime_factor);
-        for factor in number_factors {
-            let current_exponent = factorization.get(&factor.0).unwrap_or(&0);
-            factorization.insert(factor.0, *current_exponent + factor.1);
+        let mut power: u32 = 0;
+        let mut n_copy: u32 = n;
+        while n_copy > 0 {
+            n_copy /= *prime;
+            power += n_copy;
         }
+        factorization.push((*prime, power));
     }
     factorization
 }
 
-fn roundness(factorization: HashMap<u32, u32>, base_factors: Vec<(u32, u32)>) -> u32 {
-    let mut roundness: u32 = u32::MAX;
-    for factor in base_factors {
-        let factor_base = factor.0;
-        let factor_exponent = factor.1;
-        let factorization_exponent = factorization.get(&factor_base).unwrap_or(&0);
-        roundness = cmp::min(roundness, *factorization_exponent / factor_exponent);
-    }
-    roundness
-}
-
 fn main() {
-    let least_prime_factor: Vec<u32> = sieve();
-    let factorial_factorization: HashMap<u32, u32> = factorize_factorial(10, &least_prime_factor);
-    let base: u32 = 12;
-    let base_factors: Vec<(u32, u32)> = factorize(base, &least_prime_factor);
-
-    let round: u32 = roundness(factorial_factorization, base_factors);
-    println!("{round}");
+    let n: u32 = 10_000_000;
+    let mut ans: u64 = 0;
+    let primes: Vec<u32> = sieve();
+    let factorial_factorization: Vec<(u32, u32)> = factorize_factorial(n, &primes);
+    let max_possible_roundness: u32 = factorial_factorization[0].1;
+    for roundness in 1..=max_possible_roundness {
+        let mut x: u64 = 1;
+        for factor in &factorial_factorization {
+            if factor.1 < roundness {
+                break;
+            }
+            x *= factor.1 as u64 / roundness as u64 + 1;
+            x %= MOD;
+        }
+        ans += x - 1;
+        ans %= MOD;
+    }
+    println!("{ans}");
 }
